2dc68ffb4f573ed54787cc7bf0aceb18a1df1a14
Antoine Déry <antoinedery12@hotmail.fr>
2023-09-15 23:14:21 -0400

    updated eslint rules

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,11 +2,12 @@ import { ChakraProvider, theme } from '@chakra-ui/react';
 import React from 'react';
 import Router from './Routes';
 
-export const App: React.FC = () => {
+const App: React.FC = () => {
     return (
         <ChakraProvider theme={theme}>
-            <Router />    
+            <Router />  
         </ChakraProvider>
     );
 };
 
+export default App;
--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { App } from './App';
-
+import App from './App';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
