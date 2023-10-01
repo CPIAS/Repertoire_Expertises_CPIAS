bf43187fc7a867e11ca123f69f6b157ccbadb4fb
Antoine Déry <antoinedery12@hotmail.fr>
2023-10-01 11:26:15 -0400

    improved ui

--- Frontend/src/pages/membersPage/MembersPage.tsx
+++ Frontend/src/pages/membersPage/MembersPage.tsx
@@ -4,7 +4,7 @@ import Header from '../../components/header/Header';
 import Loader from '../../components/loader/Loader';
 import MemberCard from '../../components/memberCard/MemberCard';
 import { Member } from '../../models/member';
-// import mockMembers from './mockMembers.json';
+import mockMembers from './mockMembers.json';
 import axios from 'axios';
 import humps from 'humps';
 const API_HOST = process.env.REACT_APP_SERVER_URL;
@@ -12,7 +12,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const MembersPage: React.FC = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [members, setMembers] = useState<Member[]>([]);
+    const [members, setMembers] = useState<Member[]>(mockMembers);
 
     useEffect(() => {
         const fetchMembers = async () => {
