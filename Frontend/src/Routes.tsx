88cd36925792b5a45547d81562f660c5f7ab090a
Antoine Déry <antoinedery12@hotmail.fr>
2023-10-01 13:46:46 -0400

    added base_href to react router

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -3,12 +3,12 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import AboutPage from './pages/aboutPage/AboutPage';
 import HomePage from './pages/homePage/HomePage';
 import MembersPage from './pages/membersPage/MembersPage';
-import SearchResultsPage from './pages/searchResultsPage/SearchResultsPage';
 import NotFoundPage from './pages/notFoundPage/NotFoundPage';
+import SearchResultsPage from './pages/searchResultsPage/SearchResultsPage';
 
 const Router: React.FC = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.REACT_APP_BASE_HREF}>
             <Routes>
                 <Route path="/" element={<Navigate to="/accueil" replace />} />              
                 <Route path="/accueil" element={<HomePage />} />
