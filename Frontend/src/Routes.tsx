605ff948c65b1a8b586fea59a1caf6215945beeb
Antoine Déry <antoinedery12@hotmail.fr>
2023-10-01 13:58:24 -0400

    test with hashed router

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import AboutPage from './pages/aboutPage/AboutPage';
 import HomePage from './pages/homePage/HomePage';
 import MembersPage from './pages/membersPage/MembersPage';
@@ -8,7 +8,7 @@ import SearchResultsPage from './pages/searchResultsPage/SearchResultsPage';
 
 const Router: React.FC = () => {
     return (
-        <BrowserRouter basename={process.env.REACT_APP_BASE_HREF}>
+        <HashRouter basename={process.env.REACT_APP_BASE_HREF}>
             <Routes>
                 <Route path="/" element={<Navigate to="/accueil" replace />} />              
                 <Route path="/accueil" element={<HomePage />} />
@@ -17,7 +17,7 @@ const Router: React.FC = () => {
                 <Route path="/apropos" element={<AboutPage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
