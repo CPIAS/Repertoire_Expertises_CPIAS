5b440e09bb450ca17026064e74d290d355d2eb22
AhmedSabsabi <72177234+AhmedSabsabi@users.noreply.github.com>
2023-10-23 08:51:11 -0400

    [Frontend] Adjusted routing in indexing

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -7,7 +7,5 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <App />
 );
