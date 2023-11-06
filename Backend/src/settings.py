802587713cff665b082a62cf73949f1d26c9de3a
Hichem Lamraoui <hichem.lamraoui@polymtl.ca>
2023-11-06 02:13:55 +0000

    [Backend] Added a centralized settings file for server services.

--- Backend/src/settings.py
+++ Backend/src/settings.py
@@ -4,7 +4,9 @@ SERVER_SETTINGS = {
     "resources_directory": "../resources",
     "qa_llm_model": "mistral:instruct",
     "keywords_llm_model": "camembert/camembert-large",
+    "spacy_nlp": "fr_core_news_sm",
     "users_csv_file": "../resources/cleaned_expertise_extended_renamed.csv",
     "server_log_file": "../server.log",
-    "sqlite_db": "users.db"
+    "sqlite_db": "users.db",
+    "google_drive_csv_file_id": "1sdxrz2R_nCf9Q5lbmqbHZNn3eQRkEzvQ"
 }
