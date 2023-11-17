69bd0f4e3b2545265cb95c995b9deddc1937225b
Hichem Lamraoui <hichem.lamraoui@polymtl.ca>
2023-11-17 14:37:11 +0000

    [Backend] Added server settings.

--- Backend/src/settings.py
+++ Backend/src/settings.py
@@ -3,13 +3,16 @@ SERVER_SETTINGS = {
     "database_directory": "../database",
     "resources_directory": "../resources",
     "vector_directory": "../vector_store",
-    "qa_llm_model": "mistral:instruct",
+    "expert_recommendation_llm_model": "mistral:instruct",
     "keywords_llm_model": "camembert/camembert-large",
-    "spacy_nlp": "fr_core_news_sm",
+    "spacy_nlp_fr": "fr_core_news_sm",
+    "spacy_nlp_en": "en_core_web_sm",
     "users_csv_file": "../resources/users.csv",
+    "users_json_file": "../resources/users.json",
     "linkedin_data_file": "../resources/out.json",
     "access_log_file": "../server_access.log",
     "error_log_file": "../server_error.log",
     "sqlite_db": "users.db",
+    "chroma_collection_name": "Experts",
     "google_drive_csv_file_id": "1GdqBrGbhtBrvnOgeIwJWA-WP0VFzr8d9"
 }
