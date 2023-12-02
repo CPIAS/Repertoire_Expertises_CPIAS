700a2e1ec655aa02e426cda946e4ffc95a16c5b8
Hichem Lamraoui <hichem.lamraoui@polymtl.ca>
2023-12-02 22:00:38 +0000

    [Backend] Added database documentation.

--- Backend/src/database.py
+++ Backend/src/database.py
@@ -61,6 +61,20 @@ class Database:
         self.is_available: bool = False
 
     def init(self) -> None:
+        """
+            Initialize the database.
+
+            This method:
+                1. Creates the database directory if it doesn't exist.
+                2. Configures the SQLAlchemy database URI for SQLite.
+                3. Initializes and binds the SQLAlchemy database to the Flask app.
+                4. Creates all tables defined in the database model.
+                5. Populates the database with data from the specified CSV file if it is empty.
+
+            Returns:
+                None
+        """
+
         with self.app.app_context():
             try:
                 if not os.path.exists(self.__database_directory):
@@ -81,6 +95,20 @@ class Database:
                 self.app_logger.info(msg="The database has been successfully initialized.")
 
     def get_date(self, date_string: str) -> Optional[date]:
+        """
+            Convert a date string to a date object.
+
+            This method:
+                1. Parses the input date string using the format "%m/%d/%Y %H:%M:%S".
+                2. Returns the parsed date object if successful, otherwise logs an error and returns None.
+
+            Parameters:
+                date_string (str): The date string to convert.
+
+            Returns:
+                Optional[date]: The parsed date object or None if parsing fails.
+        """
+
         try:
             formatted_date = datetime.strptime(date_string, "%m/%d/%Y %H:%M:%S").date()
         except ValueError as e:
@@ -90,6 +118,21 @@ class Database:
             return formatted_date
 
     def get_number(self, number_string: str, t: Type[T]) -> Optional[T]:
+        """
+            Convert a number string to a specified type.
+
+            This method:
+                1. Parses the input number string using the provided type 't'.
+                2. Returns the parsed number object if successful, otherwise logs an error and returns None.
+
+            Parameters:
+                number_string (str): The number string to convert.
+                t (Type[T]): The target type for conversion.
+
+            Returns:
+                Optional[T]: The parsed number object of type 't' or None if parsing fails.
+        """
+
         try:
             number = t(number_string)
         except ValueError as e:
@@ -99,6 +142,22 @@ class Database:
             return number
 
     def create_user_from_csv_row(self, row: list[str]):
+        """
+            Create a User instance from a CSV row.
+
+            This method:
+                1. Extracts values from the provided CSV row using attribute-to-column mappings.
+                2. Converts date and number strings to appropriate types.
+                3. Queries LLM to get keywords based on user skills.
+                4. Constructs and returns a User instance with the extracted values.
+
+            Parameters:
+                row (list[str]): The CSV row containing user data.
+
+            Returns:
+                User: The created User instance.
+        """
+
         return User(
             registration_date=self.get_date(row[self.user_attributes_to_csv_columns_map["registration_date"]]),
             first_name=row[self.user_attributes_to_csv_columns_map["first_name"]],
@@ -119,10 +178,36 @@ class Database:
         )
 
     def is_empty(self) -> bool:
+        """
+            Check if the User table in the database is empty.
+
+            This method:
+                1. Queries the User table to retrieve all records.
+                2. Returns True if the table is empty, False otherwise.
+
+            Returns:
+                bool: True if the User table is empty, False otherwise.
+        """
+
         with self.app.app_context():
             return len(self.session.query(User).all()) == 0
 
     def populate(self, users_csv_file: str) -> None:
+        """
+            Populate the User table in the database with data from a CSV file.
+
+            This method:
+                1. Reads data from the specified CSV file, skipping the header row.
+                2. Creates a User instance for each row and adds it to the database session.
+                3. Commits the changes to persist the data in the User table.
+
+            Parameters:
+                users_csv_file (str): The path to the CSV file containing user data.
+
+            Returns:
+                None
+        """
+
         with self.app.app_context():
             data = self.read_csv(users_csv_file)
 
@@ -133,6 +218,24 @@ class Database:
             self.session.commit()
 
     def update(self, users_csv_file: str) -> None:
+        """
+            Update the User table in the database based on data from a CSV file.
+
+            This method:
+                1. Reads data from the specified CSV file, skipping the header row.
+                2. For each row:
+                    - Retrieves the corresponding User instance from the database.
+                    - Updates User attributes if values differ, including date and number conversions.
+                    - Adds a new User if not found in the database.
+                3. Commits the changes to persist the updated data in the User table.
+
+            Parameters:
+                users_csv_file (str): The path to the CSV file containing user data.
+
+            Returns:
+                None
+        """
+
         with self.app.app_context():
             try:
                 self.is_available = False
@@ -170,6 +273,21 @@ class Database:
 
     @staticmethod
     def read_csv(file_path: str) -> list[list[str]]:
+        """
+            Read data from a CSV file.
+
+            This static method:
+                1. Opens the specified CSV file in read mode.
+                2. Uses the CSV reader to iterate over rows and appends them to a list.
+                3. Returns the list containing CSV data.
+
+            Parameters:
+                file_path (str): The path to the CSV file.
+
+            Returns:
+                list[list[str]]: A list of lists, where each inner list represents a row of the CSV file.
+        """
+
         data = []
 
         with open(file_path, 'r') as file:
@@ -181,11 +299,44 @@ class Database:
 
     @staticmethod
     def write_csv(file_path: str, data: list[list[str]]) -> None:
+        """
+            Write data to a CSV file.
+
+            This static method:
+                1. Opens the specified CSV file in write mode.
+                2. Uses the CSV writer to write rows of data to the file.
+
+            Parameters:
+                file_path (str): The path to the CSV file.
+                data (list[list[str]]): A list of lists, where each inner list represents a row of data.
+
+            Returns:
+                None
+        """
+
         with open(file_path, 'w') as file:
             writer = csv.writer(file)
             writer.writerows(data)
 
     def delete_user_from_csv(self, user_email: str) -> None:
+        """
+            Delete a user's data from the CSV file.
+
+            This method:
+                1. Reads data from the users CSV file.
+                2. Creates a new list excluding the row with the specified user_email.
+                3. Writes the updated data back to the CSV file.
+
+            Parameters:
+                user_email (str): The email of the user to be deleted.
+
+            Returns:
+                None
+
+            Raises:
+                Exception: If the user is not found in the CSV file, indicating a potential inconsistency.
+        """
+
         data = self.read_csv(SERVER_SETTINGS["users_csv_file"])
         updated_data = []
 
@@ -200,6 +351,26 @@ class Database:
         self.write_csv(SERVER_SETTINGS["users_csv_file"], updated_data)
 
     def update_user_in_csv(self, user_email: str, user_information_to_be_updated: dict) -> None:
+        """
+            Update user information in the CSV file.
+
+            This method:
+                1. Reads data from the users CSV file.
+                2. Finds the user's row based on the specified user_email.
+                3. Updates the user's information with the provided dictionary.
+                4. Writes the updated data back to the CSV file.
+
+            Parameters:
+                user_email (str): The email of the user to be updated.
+                user_information_to_be_updated (dict): A dictionary containing key-value pairs of information to be updated.
+
+            Returns:
+                None
+
+            Raises:
+                Exception: If the user is not found in the CSV file, indicating a potential inconsistency.
+        """
+
         data = self.read_csv(SERVER_SETTINGS["users_csv_file"])
         user = []
 
@@ -218,6 +389,22 @@ class Database:
 
     @staticmethod
     def __get_expert_skills_from_json(json_file_path: str, expert_email: str) -> str:
+        """
+            Extract expert skills from a JSON file.
+
+            This static method:
+                1. Reads data from the specified JSON file.
+                2. Searches for the expert's email in the JSON data.
+                3. Retrieves and concatenates the descriptions of experiences related to the expert's skills.
+
+            Parameters:
+                json_file_path (str): The path to the JSON file containing expert profiles.
+                expert_email (str): The email of the expert to retrieve skills for.
+
+            Returns:
+                str: A string containing concatenated descriptions of experiences related to the expert's skills.
+        """
+
         expert_skills = ''
 
         with open(json_file_path, 'r') as json_file:
@@ -232,7 +419,22 @@ class Database:
 
         return expert_skills
 
-    def validate_csv(self, users_csv_file: str):
+    def validate_csv(self, users_csv_file: str) -> tuple[bool, str]:
+        """
+            Validate the format of a CSV file.
+
+            This method:
+                1. Reads the headers from the specified CSV file.
+                2. Checks if all required columns are present.
+                3. Verifies the correct order of columns.
+
+            Parameters:
+                users_csv_file (str): The path to the CSV file to be validated.
+
+            Returns:
+                Tuple[bool, str]: A tuple indicating whether the CSV file is valid (True/False) and a message.
+        """
+
         headers = self.read_csv(users_csv_file)[0]
 
         # Check if all required columns are present
@@ -250,6 +452,29 @@ class Database:
 
 @dataclass
 class User(Database.db.Model):
+    """
+        Represents a user in the application.
+
+        Attributes:
+            user_id (int): The unique identifier for the user (primary key).
+            first_name (str): The user's first name.
+            last_name (str): The user's last name.
+            registration_date (datetime.date): The date when the user registered (nullable).
+            email (str): The unique email address of the user (unique, not nullable).
+            membership_category (str): The category of membership for the user (nullable).
+            job_position (str): The user's job position (nullable).
+            affiliation_organization (str): The organization the user is affiliated with (nullable).
+            skills (str): The skills possessed by the user (nullable).
+            years_experience_ia (float): The years of experience in artificial intelligence (nullable).
+            years_experience_healthcare (float): The years of experience in healthcare (nullable).
+            community_involvement (str): The user's involvement in the community (nullable).
+            suggestions (str): Any suggestions provided by the user (nullable).
+            tags (str): Keywords associated with the user's skills (nullable).
+            consent (str): User's consent information (nullable).
+            profile_photo (str): The filename of the user's profile photo (nullable).
+            linkedin (str): The user's LinkedIn profile link (nullable).
+    """
+
     __tablename__ = 'users'
 
     user_id: int = Column(Integer, primary_key=True)
