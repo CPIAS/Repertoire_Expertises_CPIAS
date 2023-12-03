210eafe2195f69c8cc81b63c530801e7ab689b93
Hichem Lamraoui <hichem.lamraoui@polymtl.ca>
2023-12-03 00:24:58 +0000

    [Backend] Added wsgi.py documentation.

--- Backend/src/wsgi.py
+++ Backend/src/wsgi.py
@@ -3,6 +3,15 @@ from app import app, init_server
 
 
 def start_server() -> Flask:
+    """
+        Initializes and starts the Flask server.
+
+        This method is responsible for initializing the server using the `init_server` function and returning the Flask app instance.
+
+        Returns:
+            Flask: The initialized Flask app instance.
+    """
+
     init_server()
     return app
 
